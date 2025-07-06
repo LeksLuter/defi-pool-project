078fac25bfd3e47b478bd2f9681bad2a4480394c
knopka <knopka@users.noreply.github.com>
2025-07-07 00:31:45 +0500

    All

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config(); // <-- Добавьте эту строку
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
     amoy: {
-      url: process.env.ALCHEMY_POLYGON_AMOY_URL,
+      url: process.env.ALCHEMY_POLYGON_AMOY_URL || "https://polygon-amoy.g.alchemy.com/v2/YOUR_ALCHEMY_KEY ",
       accounts: [process.env.PRIVATE_KEY],
     },
     polygon: {
-      url: process.env.ALCHEMY_POLYGON_MAINNET_URL,
+      url: process.env.ALCHEMY_POLYGON_MAINNET_URL || "https://polygon-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_KEY ",
       accounts: [process.env.PRIVATE_KEY],
     }
   }
