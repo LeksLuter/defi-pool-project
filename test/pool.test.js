9f03910b47f707851fe398bac732174d6ea0d876
knopka <knopka@users.noreply.github.com>
2025-07-08 00:59:01 +0500

    Update pool.test.js

--- test/pool.test.js
+++ test/pool.test.js
@@ -6,26 +6,48 @@ describe("LiquidityPool", function () {
 
   beforeEach(async () => {
     [owner, user] = await ethers.getSigners();
-    const Factory = await ethers.getContractFactory("PoolFactory");
-    factory = await Factory.deploy();
-    const tx = await factory.createPool(ethers.constants.AddressZero, ethers.constants.AddressZero, 30);
+
+    // Деплой фабрики пулов
+    const PoolFactory = await ethers.getContractFactory("PoolFactory");
+    factory = await PoolFactory.deploy();
+    await factory.deployed();
+
+    // Создание пула с токенами и комиссией
+    const tokenA = ethers.constants.AddressZero; // Заглушка для токена A
+    const tokenB = ethers.constants.AddressZero; // Заглушка для токена B
+    const feeRate = 30; // 0.3%
+
+    const tx = await factory.createPool(tokenA, tokenB, feeRate);
     const receipt = await tx.wait();
     const poolAddress = receipt.events[0].args.pool;
+
+    // Подключение к созданному пулу
     pool = await ethers.getContractAt("LiquidityPool", poolAddress);
   });
 
-  it("Should add liquidity", async function () {
-    await pool.addLiquidity(0,1, 0,1, 980000, 1020000);
+  it("Should add liquidity with 0.1 tokens", async function () {
+    const amount0 = ethers.utils.parseUnits("0.1", 18); // 0.1 токена A
+    const amount1 = ethers.utils.parseUnits("0.1", 18); // 0.1 токена B
+    const lowerSqrtPrice = 980000;
+    const upperSqrtPrice = 1020000;
+
+    await pool.addLiquidity(amount0, amount1, lowerSqrtPrice, upperSqrtPrice);
+
     const reserves = await pool.getReserves();
-    expect(reserves[0]).to.equal(0,1);
+    expect(reserves[0]).to.equal(amount0);
+    expect(reserves[1]).to.equal(amount1);
   });
 
-  it("Should not allow non-owner to remove liquidity", async function () {
-    await pool.addLiquidity(0,1, 0,1, 980000, 1020000);
+  it("Should NOT allow non-owner to remove liquidity", async function () {
+    const amount0 = ethers.utils.parseUnits("0.1", 18);
+    const amount1 = ethers.utils.parseUnits("0.1", 18);
+    const lowerSqrtPrice = 980000;
+    const upperSqrtPrice = 1020000;
+
+    await pool.addLiquidity(amount0, amount1, lowerSqrtPrice, upperSqrtPrice);
     const tokenId = 0;
 
-    await expect(pool.connect(user).removeLiquidity(tokenId)).to.be.revertedWith(
-      "Only owner can remove liquidity"
-    );
+    await expect(pool.connect(user).removeLiquidity(tokenId))
+      .to.be.revertedWith("Only owner can remove liquidity");
   });
 });
