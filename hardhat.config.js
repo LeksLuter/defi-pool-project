5d7ecca1fcf8ff57fcbcf89aed1101c2916d283d
knopka <lexsis_alaska@mail.ru>
2025-07-17 21:22:00 +0500

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config(); // <-- Добавьте эту строку
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    amoy: {
-      url: process.env.ALCHEMY_POLYGON_AMOY_URL || "https://polygon-amoy.g.alchemy.com/v2/YOUR_ALCHEMY_KEY ",
-      accounts: [process.env.PRIVATE_KEY],
-    },
     polygon: {
-      url: process.env.ALCHEMY_POLYGON_MAINNET_URL || "https://polygon-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_KEY ",
-      accounts: [process.env.PRIVATE_KEY],
+      url: process.env.ALCHEMY_POLYGON_MAINNET_URL,
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
