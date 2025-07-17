9576bf6953f4f271a2b423ebe8d6b1a8e028aa09
knopka <lexsis_alaska@mail.ru>
2025-07-17 18:33:51 +0500

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Web3Provider } from "./context/Web3Context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Web3Provider>
+  <React.StrictMode>
     <App />
-  </Web3Provider>
+  </React.StrictMode>
 );
